e4f3e58ef8a6df7a9d989556c06ad1ccd716c41d
kouldep <kouldep5031@gmail.com>
2021-07-03 07:57:46 +0530

    Adding line

--- testscript.sh
+++ testscript.sh
@@ -3,6 +3,5 @@ echo "This is a test script"
 Num=$(($FirstN+$SecondN))
 echo "Sum is :" $Num
 echo "This Completes the Script"
-echo "Adding this line after enablin hook"
 echo "This is Magic"
 echo "This auto build"
